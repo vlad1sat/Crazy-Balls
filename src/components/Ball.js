cc436e5970bb413e037db4cb8d023d509f5aa278
vlad1sat <pyatkov200316@mail.ru>
2023-02-25 14:42:54 +0500

    add random position

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,8 +1,8 @@
 import React from "react";
 import smile from "../img/smile.png";
-import bad from "../img/bad.png"
+import bad from "../img/bad.png";
 
-export default function Ball( { mood }) {
+export default function Ball({ mood }) {
     const mouseDownBall = evt => {
         const smile = evt.target.parentElement.nodeName === 'DIV'
             ? evt.target.parentElement
@@ -25,8 +25,19 @@ export default function Ball( { mood }) {
         smile.ondragstart = () => false;
     }
 
+    const getPositionElement = (sizeWindow) => {
+        let coordinate = Math.random() * sizeWindow - 220;
+        if (coordinate < 0) coordinate = 0;
+        return coordinate;
+    }
+
+    const positionElement = {
+        left: getPositionElement(window.innerWidth),
+        top: getPositionElement(window.innerHeight)
+    }
+
     return (
-        <div className="ball" onMouseDown={mouseDownBall} >
+        <div className="ball" onMouseDown={mouseDownBall} style={positionElement}>
             <img src={mood ? smile : bad} alt="ball"/>
         </div>
     );
